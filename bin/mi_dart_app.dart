4e1eb07cb18fe86be1d4ccffd6ccf9e654257895
Darking01 <enrique.fuentes@unitec.edu>
2025-05-24 14:49:52 -0600

    Multi-Div
    Se realizo la division y la multiplicacion

--- bin/mi_dart_app.dart
+++ bin/mi_dart_app.dart
@@ -19,18 +19,91 @@ void main(List<String> arguments) {
   opcion = stdin.readLineSync() ?? "0";
 
   switch (opcion) {
+    //suma 
     case "1":
-      // Crear un nuevo usuario
+    print("========== SUMA ==========");
+    print("Ingrese el primer número:");
+      String? input1 = stdin.readLineSync();
+      print("Ingrese el segundo número:");
+      String? input2 = stdin.readLineSync();
+      // Validar que las entradas no sean nulas y convertirlas a enteros
+      if (input1 != null && input2 != null) {
+        double num1 = double.parse(input1);
+        double num2 = double.parse(input2);
+        double resultado = num1 + num2;
+        print("El resultado de la suma es: $resultado");
+      } else {
+        print("Entrada no válida.");
+
+
+      }
       break;
+    
+    //resta
     case "2":
+     print("========== Resta ==========");
+    print("Ingrese el primer número:");
+      String? input1 = stdin.readLineSync();
+      print("Ingrese el segundo número:");
+      String? input2 = stdin.readLineSync();
+      // Validar que las entradas no sean nulas y convertirlas a enteros
+      if (input1 != null && input2 != null) {
+        double num1 = double.parse(input1);
+        double num2 = double.parse(input2);
+        double resultado = num1 - num2;
+        print("El resultado de la resta  es: $resultado");
+      } else {
+        print("Entrada no válida.");
+
+
+      }
       // Mostrar todos los usuarios
+
       break;
     case "3":
-      // Buscar un usuario
+      // MULTIPLICACION
+      print("========== Multiplicacion ==========");
+      // Solicitar dos números al usuario
+      print("Ingrese el primer número:");
+      String? input1 = stdin.readLineSync();
+      print("Ingrese el segundo número:");
+      String? input2 = stdin.readLineSync();
+      // Validar que las entradas no sean nulas y convertirlas a enteros
+      if (input1 != null && input2 != null) {
+        int num1 = int.parse(input1);
+        int num2 = int.parse(input2);
+        int resultado = num1 * num2;
+        print("El resultado de la multiplicación es: $resultado");
+      } else {
+        print("Entrada no válida.");
+      }
       break;
+
     case "4":
-      // Actualizar un usuario
+      // DIVISION
+      print("========== Division ==========");
+      // Solicitar dos números al usuario
+      print("Ingrese el primer número:");
+      String? input1 = stdin.readLineSync();
+      print("Ingrese el segundo número:");
+      String? input2 = stdin.readLineSync();
+
+      // Validar que las entradas no sean nulas y convertirlas a enteros
+      if (input1 != null && input2 != null) {
+        int num1 = int.parse(input1);
+        int num2 = int.parse(input2);
+        // Validar que el segundo número no sea cero
+        if (num2 != 0) {
+          double resultado = num1 / num2;
+          print("El resultado de la división es: $resultado");
+        } else {
+          print("Error: No se puede dividir entre cero.");
+        }
+      } else {
+        print("Entrada no válida.");
+      }
       break;
+
     case "5":
       // Eliminar un usuario
       break;
