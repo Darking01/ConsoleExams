4d745ae76c7d18b8370022c8abc5f5fb69851f67
Darking01 <enrique.fuentes@unitec.edu>
2025-05-24 14:49:52 -0600

    Multi-Div
    Se realizo la division y la multiplicacion

--- bin/mi_dart_app.dart
+++ bin/mi_dart_app.dart
@@ -24,13 +24,52 @@ void main(List<String> arguments) {
       break;
     case "2":
       // Mostrar todos los usuarios
+
       break;
     case "3":
-      // Buscar un usuario
+      // MULTIPLICACION
+      print("========== Multiplicacion ==========");
+      // Solicitar dos números al usuario
+      print("Ingrese el primer número:");
+      String? input1 = stdin.readLineSync();
+      print("Ingrese el segundo número:");
+      String? input2 = stdin.readLineSync();
+      // Validar que las entradas no sean nulas y convertirlas a enteros
+      if (input1 != null && input2 != null) {
+        int num1 = int.parse(input1);
+        int num2 = int.parse(input2);
+        int resultado = num1 * num2;
+        print("El resultado de la multiplicación es: $resultado");
+      } else {
+        print("Entrada no válida.");
+      }
       break;
+
     case "4":
-      // Actualizar un usuario
+      // DIVISION
+      print("========== Division ==========");
+      // Solicitar dos números al usuario
+      print("Ingrese el primer número:");
+      String? input1 = stdin.readLineSync();
+      print("Ingrese el segundo número:");
+      String? input2 = stdin.readLineSync();
+
+      // Validar que las entradas no sean nulas y convertirlas a enteros
+      if (input1 != null && input2 != null) {
+        int num1 = int.parse(input1);
+        int num2 = int.parse(input2);
+        // Validar que el segundo número no sea cero
+        if (num2 != 0) {
+          double resultado = num1 / num2;
+          print("El resultado de la división es: $resultado");
+        } else {
+          print("Error: No se puede dividir entre cero.");
+        }
+      } else {
+        print("Entrada no válida.");
+      }
       break;
+
     case "5":
       // Eliminar un usuario
       break;
